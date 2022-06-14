723c7b9456841f730002f6acb4d5918f82cf35c6
Plaicio <101359111+Plaicio@users.noreply.github.com>
2022-06-14 12:11:44 -0400

    Add files via upload
    La muricion*

---
+++ andres.py
@@ -0,0 +1,32 @@
+while True:
+    print("0.menu")
+    print("1.comenzar el programa")
+    print("2.imprimir listado")
+    print("3.finalizar programa")
+
+    opcion= 0
+    int(input("que opcion deseas ejecutar? : "))
+
+if opcion == 1:
+    print("programa iniciado")
+
+elif opcion == 2:
+    print("escriba su listado:")
+    print("manzana,platano,naranja")
+
+elif opcion == 3:
+    print("has finalizado el programa")
+
+
+
+
+else:
+
+    print("error")
+
+
+
+
+
+
+
